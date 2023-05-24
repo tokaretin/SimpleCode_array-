38b69284c8f7f2a7ef44288ad0b081af050bb376
tokaretin <tokaretin@gmail.com>
2023-05-24 14:23:12 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode_array[]/SimpleCode_array[].cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int SIZE = 10;
+	int arr[SIZE];
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		int num;
+		std::cin >> num;
+		arr[i] = num;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << arr[i] << ", ";
+	}
+}
+
+ 
